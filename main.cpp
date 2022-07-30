41e0d32915bb7cc84601ec885a050e80ceb8d5a9
FredrickWampamba <fredrickwampamba1@gmail.com>
2022-07-30 15:18:56 +0300

    files

--- main.cpp
+++ main.cpp
@@ -1,7 +1,8 @@
+#include <winsock2.h>
 #include <windows.h>
-#include <iostream>
-#include <fstream>
-#include <string.h>
+
+#include <iphlpapi.h>
+#include <stdio.h>
 using namespace std;
 
 /* This is where all the input to the window goes to */
@@ -57,44 +58,218 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
 		return 0;
 	}
 	
-	string line;
-	ifstream IPFile;
-	int offset; 
-	char* search0 = "IPv4 Address. . . . . . . . . . . :";      // search pattern
-	        
-	system("ipconfig > ip.txt");
+	DWORD Err;
+
+    PFIXED_INFO pFixedInfo;
+    DWORD FixedInfoSize = 0;
+
+    PIP_ADAPTER_INFO pAdapterInfo, pAdapt;
+    DWORD AdapterInfoSize;
+    PIP_ADDR_STRING pAddrStr;
+
+    UINT i;
+
+    struct tm newtime;
+    char buffer[32];    
+    errno_t error;
+
+    //
+    // Get the main IP configuration information for this machine using a FIXED_INFO structure
+    //
+    if ((Err = GetNetworkParams(NULL, &FixedInfoSize)) != 0)
+    {
+        if (Err != ERROR_BUFFER_OVERFLOW)
+        {
+            printf("GetNetworkParams sizing failed with error %d\n", Err);
+            return 0;
+        }
+    }
+
+    // Allocate memory from sizing information
+    if ((pFixedInfo = (PFIXED_INFO) GlobalAlloc(GPTR, FixedInfoSize)) == NULL)
+    {
+        printf("Memory allocation error\n");
+        return 0;
+    }
+
+    if ((Err = GetNetworkParams(pFixedInfo, &FixedInfoSize)) == 0)
+    {
+        printf("\tHost Name . . . . . . . . . : %s\n", pFixedInfo->HostName);
+        printf("\tDNS Servers . . . . . . . . : %s\n", pFixedInfo->DnsServerList.IpAddress.String);
+        pAddrStr = pFixedInfo->DnsServerList.Next;
+        while(pAddrStr)
+        {
+            printf("%51s\n", pAddrStr->IpAddress.String);
+            pAddrStr = pAddrStr->Next;
+        }
+
+        printf("\tNode Type . . . . . . . . . : ");
+        switch (pFixedInfo->NodeType)
+        {
+            case 1:
+                printf("%s\n", "Broadcast");
+                break;
+            case 2:
+                printf("%s\n", "Peer to peer");
+                break;
+            case 4:
+                printf("%s\n", "Mixed");
+                break;
+            case 8:
+                printf("%s\n", "Hybrid");
+                break;
+            default:
+                printf("\n");
+        }
+
+        printf("\tNetBIOS Scope ID. . . . . . : %s\n", pFixedInfo->ScopeId);
+        printf("\tIP Routing Enabled. . . . . : %s\n", (pFixedInfo->EnableRouting ? "yes" : "no"));
+        printf("\tWINS Proxy Enabled. . . . . : %s\n", (pFixedInfo->EnableProxy ? "yes" : "no"));
+        printf("\tNetBIOS Resolution Uses DNS : %s\n", (pFixedInfo->EnableDns ? "yes" : "no"));
+    } else
+    {
+        printf("GetNetworkParams failed with error %d\n", Err);
+        return 0;
+    }
+
+    //
+    // Enumerate all of the adapter specific information using the IP_ADAPTER_INFO structure.
+    // Note:  IP_ADAPTER_INFO contains a linked list of adapter entries.
+    //
+    AdapterInfoSize = 0;
+    if ((Err = GetAdaptersInfo(NULL, &AdapterInfoSize)) != 0)
+    {
+        if (Err != ERROR_BUFFER_OVERFLOW)
+        {
+            printf("GetAdaptersInfo sizing failed with error %d\n", Err);
+            return 0;
+        }
+    }
+
+    // Allocate memory from sizing information
+    if ((pAdapterInfo = (PIP_ADAPTER_INFO) GlobalAlloc(GPTR, AdapterInfoSize)) == NULL)
+    {
+        printf("Memory allocation error\n");
+        return 0;
+    }
+
+    // Get actual adapter information
+    if ((Err = GetAdaptersInfo(pAdapterInfo, &AdapterInfoSize)) != 0)
+    {
+        printf("GetAdaptersInfo failed with error %d\n", Err);
+        return 0;
+    }
+
+    pAdapt = pAdapterInfo;
+
+    while (pAdapt)
+    {
+        switch (pAdapt->Type)
+        {
+            case MIB_IF_TYPE_ETHERNET:
+                printf("\nEthernet adapter ");
+                break;
+            case MIB_IF_TYPE_TOKENRING:
+                printf("\nToken Ring adapter ");
+                break;
+            case MIB_IF_TYPE_FDDI:
+                printf("\nFDDI adapter ");
+                break;
+            case MIB_IF_TYPE_PPP:
+                printf("\nPPP adapter ");
+                break;
+            case MIB_IF_TYPE_LOOPBACK:
+                printf("\nLoopback adapter ");
+                break;
+            case MIB_IF_TYPE_SLIP:
+                printf("\nSlip adapter ");
+                break;
+            case MIB_IF_TYPE_OTHER:
+            default:
+                printf("\nOther adapter ");
+        }
+        printf("%s:\n\n", pAdapt->AdapterName);
+
+        printf("\tDescription . . . . . . . . : %s\n", pAdapt->Description); 
+
+        printf("\tPhysical Address. . . . . . : ");
+	 
+        for (i=0; i<pAdapt->AddressLength; i++)
+        {
+            if (i == (pAdapt->AddressLength - 1))
+                printf("%.2X\n",(int)pAdapt->Address[i]);
+            else
+                printf("%.2X-",(int)pAdapt->Address[i]);
+        }        
+
+        printf("\tDHCP Enabled. . . . . . . . : %s\n", (pAdapt->DhcpEnabled ? "yes" : "no"));
+
+        pAddrStr = &(pAdapt->IpAddressList);
+        while(pAddrStr)
+        {
+            printf("\tIP Address. . . . . . . . . : %s\n", pAddrStr->IpAddress.String);
+            printf("\tSubnet Mask . . . . . . . . : %s\n", pAddrStr->IpMask.String);
+            pAddrStr = pAddrStr->Next;
+        }
+
+        printf("\tDefault Gateway . . . . . . : %s\n", pAdapt->GatewayList.IpAddress.String);
+        pAddrStr = pAdapt->GatewayList.Next;
+        while(pAddrStr)
+        {
+            printf("%51s\n", pAddrStr->IpAddress.String);
+            pAddrStr = pAddrStr->Next;
+        }
+
+        printf("\tDHCP Server . . . . . . . . : %s\n", pAdapt->DhcpServer.IpAddress.String);
+        printf("\tPrimary WINS Server . . . . : %s\n", pAdapt->PrimaryWinsServer.IpAddress.String);
+        printf("\tSecondary WINS Server . . . : %s\n", pAdapt->SecondaryWinsServer.IpAddress.String);
+
+        
+        if (error)
+        {
+            printf("Invalid Argument to _localtime32_s.");
+        } else {
+            // Convert to an ASCII representation 
+            
+            if (error)
+            {
+                printf("Invalid Argument to asctime_s.");
+            } else {  
+                printf( "\tLease Obtained. . . . . . . : %s", buffer);
+            }
+        }
+
+        if (error)
+        {
+            printf("Invalid Argument to _localtime32_s.");
+        } else {
+            if (error)
+            {
+                printf("Invalid Argument to asctime_s.");
+            } else { 
+                printf( "\tLease Expires . . . . . . . : %s", buffer);
+            }
+        }
+
+        pAdapt = pAdapt->Next;
+    }
 	
-	IPFile.open ("ip.txt"); 
-	if(IPFile.is_open())
-	{
-	   while(!IPFile.eof())
-	   {
-	   getline(IPFile,line);
-	   if ((offset = line.find(search0, 0)) != string::npos)
-	   {
-	//   IPv4 Address. . . . . . . . . . . : 1
-	//1234567890123456789012345678901234567890     
-	   line.erase(0,39);
-	   IPFile.close();
-	   }
-	}
-	}
 	
-	HDC hdc = GetDC(hwnd);
-	RECT rect;
-	GetClientRect(hwnd, &rect);
-//	char * text = line;
-	DrawTextA(hdc, line, line.length(), &rect, DT_CENTER);
-	ReleaseDC(hwnd,hdc);
+//	HDC hdc = GetDC(hwnd);
+//	RECT rect;
+//	GetClientRect(hwnd, &rect);
+//	char * text = (char *)line;
+//	DrawTextA(hdc, text, strlen(text), &rect, DT_CENTER);
+//	ReleaseDC(hwnd,hdc);
 
 	/*
 		This is the heart of our program where all input is processed and 
 		sent to WndProc. Note that GetMessage blocks code flow until it receives something, so
 		this loop will not produce unreasonably high CPU usage
 	*/
-	while(GetMessage(&msg, NULL, 0, 0) > 0) { /* If no error is received... */
-		TranslateMessage(&msg); /* Translate key codes to chars if present */
-		DispatchMessage(&msg); /* Send it to WndProc */
-	}
-	return msg.wParam;
+//	while(GetMessage(&msg, NULL, 0, 0) > 0) { /* If no error is received... */
+//		TranslateMessage(&msg); /* Translate key codes to chars if present */
+//		DispatchMessage(&msg); /* Send it to WndProc */
+//	}
+//	return msg.wParam;
 }
