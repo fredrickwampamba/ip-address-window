abdcdf0a776753e51a5fbe671794d752207e9ac8
FredrickWampamba <fredrickwampamba1@gmail.com>
2022-08-04 16:11:30 +0300

    make files, res files, etc

---
+++ ip_address_private.h
@@ -0,0 +1,23 @@
+/* THIS FILE WILL BE OVERWRITTEN BY DEV-C++ */
+/* DO NOT EDIT ! */
+
+#ifndef IP_ADDRESS_PRIVATE_H
+#define IP_ADDRESS_PRIVATE_H
+
+/* VERSION DEFINITIONS */
+#define VER_STRING	"0.4.3.2"
+#define VER_MAJOR	0
+#define VER_MINOR	4
+#define VER_RELEASE	3
+#define VER_BUILD	2
+#define COMPANY_NAME	"Kwaug Technologies"
+#define FILE_VERSION	"0.43"
+#define FILE_DESCRIPTION	"Developed using the Dev-C++ IDE"
+#define INTERNAL_NAME	""
+#define LEGAL_COPYRIGHT	"Kwaug"
+#define LEGAL_TRADEMARKS	""
+#define ORIGINAL_FILENAME	""
+#define PRODUCT_NAME	"MY IP ADDRESS"
+#define PRODUCT_VERSION	""
+
+#endif /*IP_ADDRESS_PRIVATE_H*/
